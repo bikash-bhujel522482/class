b49b1dd2099d14bd43e6ff6ed7237e4b6075e5a8
EvilByte <080csit12.bikash@scst.edu.np>
2025-03-04 22:25:00 +0545

    menu added

--- DSA/Array/a.c
+++ DSA/Array/a.c
@@ -1,19 +1,58 @@
 #include<stdio.h>
-int main()
+#include<stdlib.h>
+int a[50],size,i;
+void menu()
 {
-    int a[6], size, i;
-    
+    int op;
+    while(1)
+    {
+        printf("MENU:");
+        printf("\n1.Create Array\n2.Insertion\n3.Deletion\n4.Display\n5.Exit");
+        printf("\nEnter your option:");
+        scanf("%d",&op);
+        switch(op)
+        {
+        case 1:
+            arr();
+            break;
+        case 2:
+            ins();
+            break;
+        case 3:
+            
+            break;
+        case 4:
+            dis();
+            break;
+        case 5:
+            printf("exiting the program . . .");
+            exit(0);
+        }
+    }
+}
+void arr()
+{
     printf("Enter the size of array:");
     scanf("%d",&size);
-    printf("Enter element in array:");
+}
+void ins()
+{
+    printf("Enter element in array:\n");
     for(i=0;i<size;i++)
     {
         scanf("%d",&a[i]);
     }
+}
+void dis()
+{
     printf("Elements of array are:\n");
     for(i=0;i<size;i++)
     {
-        printf("%d",a[i]);
+        printf("%d\n",a[i]);
     }    
+}
+int main()
+{
+    menu();    
     return 0;
 }
